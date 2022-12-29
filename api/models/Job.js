9c12dfdec4f3898ce07a38e22ef78568fe965872
nmemarcoding <nmemarcoding@gmail.com>
2022-12-29 12:56:26 -0800

    methods.isBusinessOpen added to job schema and pst order rout changed base on that

--- api/models/Job.js
+++ api/models/Job.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const user = require("./User");
+const moment = require('moment');
 
 
 const JobSchema = new mongoose.Schema({
@@ -40,6 +41,33 @@ const JobSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 JobSchema.index({ location: "2dsphere" });
+
+JobSchema.methods.isBusinessOpen = function isBusinessOpen(day, time) {
+    // Convert the day of the week to a lowercase string (e.g., "Monday" -> "monday")
+    day = day.toLowerCase();
+
+    // Find the appropriate field in the schema based on the day of the week
+    const schedule = this[day];
+
+    // Check if the business is open at the current time
+    if (schedule) {
+        for (let i = 0; i < schedule.length; i += 2) {
+            // Create moment objects for the opening and closing times
+            const openingTime = moment(schedule[i], "HH:mm");
+            const closingTime = moment(schedule[i + 1], "HH:mm");
+
+            // Create a moment object for the current time
+            const times = moment(time, "HH:mm");
+
+            // Check if the current time falls within the opening and closing times
+            if (times.isBetween(openingTime, closingTime, null, "[]")) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
 
 module.exports = mongoose.model("Job", JobSchema);
 
--- api/routes/order.js
+++ api/routes/order.js
@@ -1,37 +1,39 @@
 const Job = require('../models/Job');
 const Order = require('../models/Order');
 const router = require("express").Router();
+const moment = require('moment');
+
 
 // creat order
-router.post("/", async(req, res) => {
+// router.post("/", async(req, res) => {
 
 
-    try {
-        const job = await Job.find({
-            _id: req.body.job,
-            [req.body.day]: { "$in": [req.body.time] }
-        })
+//     try {
+//         const job = await Job.find({
+//             _id: req.body.job,
+//             [req.body.day]: { "$in": [req.body.time] }
+//         })
 
-        if (job.length) {
-            const avaliableTime = await Order.find({ job: req.body.job, time: req.body.time, day: req.body.day })
-            if (!avaliableTime.length) {
-                const newOrder = new Order({...req.body, owner: job[0].owner.toString() });
-                const savedOrder = await newOrder.save();
-                res.status(200).json(savedOrder);
-            } else {
-                res.status(200).json(`${req.body.day} ${req.body.time} is tooken look for an other time`);
-            }
+//         if (job.length) {
+//             const avaliableTime = await Order.find({ job: req.body.job, time: req.body.time, day: req.body.day })
+//             if (!avaliableTime.length) {
+//                 const newOrder = new Order({...req.body, owner: job[0].owner.toString() });
+//                 const savedOrder = await newOrder.save();
+//                 res.status(200).json(savedOrder);
+//             } else {
+//                 res.status(200).json(`${req.body.day} ${req.body.time} is tooken look for an other time`);
+//             }
 
-        } else {
-            res.status(200).json(`change your time there is no avalable job at ${req.body.day} ${req.body.time}`);
-        }
-        // res.status(200).json(job);
+//         } else {
+//             res.status(200).json(`change your time there is no avalable job at ${req.body.day} ${req.body.time}`);
+//         }
+//         // res.status(200).json(job);
 
-    } catch (err) {
-        console.log(err)
-        res.status(500).json(err.message);
-    }
-});
+//     } catch (err) {
+//         console.log(err)
+//         res.status(500).json(err.message);
+//     }
+// });
 
 // Get order with customer id 
 router.get("/find/customer/:id", async(req, res) => {
@@ -48,11 +50,61 @@ router.get("/find/customer/:id", async(req, res) => {
 
 router.get("/find/owner/:id", async(req, res) => {
     try {
-        const jobs = await Order.find({ owner: req.params.id }).populate("job").populate("customer", "username email");
+        const jobs = await Order.find({ owner: req.params.id }).populate("job").populate("customer", "username email profile_image");
 
         res.status(200).json(jobs);
     } catch (err) {
 
+        res.status(500).json(err.message);
+    }
+});
+
+
+// test
+router.post("/", async(req, res) => {
+    try {
+        // Find the job associated with the order
+        const job = await Job.findById(req.body.job);
+
+        // Check if the business is open at the specified time
+        const isOpen = job.isBusinessOpen(req.body.day, req.body.time);
+        if (!isOpen) {
+            return res.status(400).json({ message: "The business is not open at the specified time." });
+        }
+
+        // Check if there are any existing orders at the specified time
+        const existingOrders = await Order.find({
+            $and: [{
+                    $or: [{
+                            time: {
+                                $gte: moment(req.body.time, "HH:mm").subtract(1, "hours").format("HH:mm"),
+                                $lte: req.body.time,
+                            },
+                            day: req.body.day,
+                        },
+                        {
+                            time: {
+                                $gte: req.body.time,
+                                $lte: moment(req.body.time, "HH:mm").add(1, "hours").format("HH:mm"),
+                            },
+                            day: req.body.day,
+                        },
+                    ],
+                },
+                { job: req.body.job },
+            ],
+        });
+        if (existingOrders.length > 0) {
+            return res.status(400).json({ message: "There is already an order at the specified time." });
+        }
+
+        const newOrder = new Order({...req.body, owner: job.owner });
+        const savedOrder = await newOrder.save();
+        res.status(200).json(savedOrder);
+
+
+    } catch (err) {
+        console.log(err)
         res.status(500).json(err.message);
     }
 });
