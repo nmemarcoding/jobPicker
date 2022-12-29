c4ed6c5ca3e737e6d246873556836e79c66df26e
nmemarcoding <nmemarcoding@gmail.com>
2022-12-29 10:46:51 -0800

    fixed some errore

--- client/src/pages/HomePage/OwnerJobs/OwnerJobs.js
+++ client/src/pages/HomePage/OwnerJobs/OwnerJobs.js
@@ -89,7 +89,7 @@ export default function OwnerOrders() {
                             </th>
                             <div className="modal" id={`${data?._id}`}>
                                 <div className="modal-box">
-                                    <h3 className="font-bold text-lg">{data.job.title}</h3>
+                                    <h3 className="font-bold text-lg">{data?.job?.title}</h3>
                                     <p className="py-4">{data?.job?.desc}</p>
                                     <div className="modal-action">
                                     <a href="#" className="btn">Colse</a>
