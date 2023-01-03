689dfc271d240c15dfa0e10fd0b7bf80da85873a
nmemarcoding <nmemarcoding@gmail.com>
2023-01-03 10:31:21 -0800

    type submit added to form btn

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -31,7 +31,7 @@ const HomePage = () => {
                     <div className="form-control">
                     <form className="input-group" >
                             <input type="text" placeholder="Search…" className="input input-bordered" style={{width:"100%"}} onChange={(e)=> setSearchData(e.target.value)}/>
-                            <button className="btn btn-square" onClick={search}>
+                            <button className="btn btn-square" onClick={search} type="submit">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                             </button>
                     </form>
