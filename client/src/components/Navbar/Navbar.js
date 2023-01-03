cab1816177ab60d79e3422db2fb58d2794fd12a7
nmemarcoding <nmemarcoding@gmail.com>
2023-01-03 11:37:34 -0800

    on mobile view search bar on the nav bar is getting of of navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -53,7 +53,7 @@ export default function Navbar() {
         
       </div>
       <div className="navbar-end">
-        <form >
+        <form className="hidden md:block" >
           <input type="text" className="p-2" onChange={(e)=> setSearchData(e.target.value)}/>
           <button className="btn btn-ghost btn-circle" onClick={() =>  navigate(`/search/${searchData}`) }>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
